2d4fa47a39b0857bfb0bba715569c83ad075eeaf
Will Roberts <willroberts3@protonmail.com>
2023-04-23 00:19:04 -0400

    use attributes instead of globals

--- lib/background.rb
+++ lib/background.rb
@@ -2,24 +2,23 @@
 
 require 'gosu'
 
-BACKGROUND_SCALE = 0.7032 # Scales 1024px to 720px. TODO: Make this dynamic.
-BACKGROUND_SPEED = 2.0
-
 module Background
   def self.initialize
     @sprite = Gosu::Image.new('sprites/background/colored_grass.png', tileable: true)
+    @scale = 0.7032 # Scales 1024px to 720px. TODO: Make this dynamic.
     @pos_x = 0
+    @speed = 2.0
   end
 
   def self.move_left
-    @pos_x -= BACKGROUND_SPEED
+    @pos_x -= @speed
   end
 
   def self.move_right
-    @pos_x += BACKGROUND_SPEED
+    @pos_x += @speed
   end
 
   def self.draw
-    @sprite.draw(@pos_x, 0, ZOrder::BACKGROUND, BACKGROUND_SCALE, BACKGROUND_SCALE)
+    @sprite.draw(@pos_x, 0, ZOrder::BACKGROUND, @scale, @scale)
   end
 end
--- lib/environment.rb
+++ lib/environment.rb
@@ -2,24 +2,23 @@
 
 require 'gosu'
 
-ENVIRONMENT_SCALE = 0.5625 # 1280px to 720px. TODO: Make this dynamic.
-ENVIRONMENT_SPEED = 4.0
-
 module Environment
   def self.initialize
     @sprite = Gosu::Image.new('sprites/levels/level1.png', tileable: false)
+    @scale = 0.5625 # 1280px to 720px. TODO: Make this dynamic.
+    @speed = 4.0
     @pos_x = 0
   end
 
   def self.move_left
-    @pos_x -= ENVIRONMENT_SPEED
+    @pos_x -= @speed
   end
 
   def self.move_right
-    @pos_x += ENVIRONMENT_SPEED
+    @pos_x += @speed
   end
 
   def self.draw
-    @sprite.draw(@pos_x, 0, ZOrder::ENVIRONMENT, ENVIRONMENT_SCALE, ENVIRONMENT_SCALE)
+    @sprite.draw(@pos_x, 0, ZOrder::ENVIRONMENT, @scale, @scale)
   end
 end
